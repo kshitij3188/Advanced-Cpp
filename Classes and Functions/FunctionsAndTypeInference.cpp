45f1e34a39c0bbdd77feec1d4f76af9f505e62c0
kshitij3188 <kshitijk3188@gmail.com>
2021-07-22 11:36:38 +0530

    Class and Functions

---
+++ Classes and Functions/FunctionsAndTypeInference.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+void print(T n)
+{
+    cout << "Template version: " << n << endl;
+}
+
+void print(int value)
+{
+    cout << "Non-template version: " << value << endl;
+}
+
+template <class T>
+void show()
+{
+    cout << T() << endl;
+}
+
+int main()
+{
+    print<string>("Hello");
+    print<int>(5);
+
+    print("Hi there");
+
+    print(5);
+    print<>(6);
+
+    show<double>();
+
+    return 0;
+}
---
+++ Classes and Functions/TemplateFunctions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+void print(T n)
+{
+    cout << n << endl;
+}
+
+int main()
+{
+    print<string>("Hello");
+    print<int>(5);
+
+    print("Hi there");
+    return 0;
+}
